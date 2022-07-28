ad0d81a23634ac4dc83a474812b40ea5c35450da
jomoke814 <jcullen814@gmail.com>
2022-07-28 10:00:46 +0300

    Adding computer and Play function

--- js/game.js
+++ js/game.js
@@ -1,1 +1,38 @@
-console.log("Hello Console!!!");
+function getComputerChoice() {
+    let rand = Math.random();
+    if(rand < 0.34) {
+        return 'rock';
+    } else if(rand <= 0.67) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return 'draw';
+    } else if(playerSelection === 'rock') {
+        if(computerSelection === 'paper') {
+            return 'Computer'
+        } else {
+            return 'Player'
+        }
+    } else if(playerSelection === 'paper') {
+        if(computerSelection === 'scissors') {
+            return 'Computer'
+        } else {
+            return 'Player'
+        }
+    } else if(playerSelection === 'scissors') {
+        if(computerSelection === 'rock') {
+            return 'Computer'
+        } else {
+            return 'Player'
+        }
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));

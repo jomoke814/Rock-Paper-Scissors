2b7f1db2839bdef096228d11c0ac7dbcb8503156
jomoke814 <jcullen814@gmail.com>
2022-07-28 18:55:02 +0300

    Adding styles and changing winners fuction

--- js/game.js
+++ js/game.js
@@ -1,38 +1,90 @@
-function getComputerChoice() {
-    let rand = Math.random();
-    if(rand < 0.34) {
-        return 'rock';
-    } else if(rand <= 0.67) {
-        return 'paper';
+const choices = ["rock", "paper", "scissors"];
+const winners = [];
+
+function game() {
+    for(let i = 0; i <= 5; i++) {
+        playRound(i);
+    }
+    document.querySelector('button').textContent = "Play new game";
+    getWins();
+}
+
+function playRound(round) {
+    const playerSelection = playerChoice();
+    const computerSelection = getComputerChoice();
+    const winner = getWinner(playerSelection, computerSelection);
+    winners.push(winner);
+    getRound(playerSelection,computerSelection,winner,round)
+}
+
+function playerChoice() {
+    let input = prompt('Type in your choice: ');
+    while (input == null){
+        input = prompt("Type in your choice: ")
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false) {
+        input = prompt('Please check your spelling');
+        while (input == null){
+            input = prompt("Type in your choice: ");
+        }
+        input = input.toLowerCase();
+        check = validateInput(input);
+    }
+    return input;
+}
+// validation of input
+function validateInput(choice) {
+    if(choices.includes(choice)){
+        return true;
     } else {
-        return 'scissors';
+        return false;
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
-        return 'draw';
-    } else if(playerSelection === 'rock') {
-        if(computerSelection === 'paper') {
-            return 'Computer'
-        } else {
-            return 'Player'
-        }
-    } else if(playerSelection === 'paper') {
-        if(computerSelection === 'scissors') {
-            return 'Computer'
-        } else {
-            return 'Player'
-        }
-    } else if(playerSelection === 'scissors') {
-        if(computerSelection === 'rock') {
-            return 'Computer'
-        } else {
-            return 'Player'
-        }
-    }
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function getWinner(p, c) {
+    if (p === c) {
+      return 'draw';
+    } else if (p === 'rock') {
+      if (c === 'paper') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
+    } else if (p === 'paper') {
+      if (c === 'scissors') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
+    } else if (p === 'scissors') {
+      if (c === 'rock') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
+    }
+  }
+
+  function getWins() {
+    let playerWins = winners.filter((item) => item == 'player').length
+    let computerWins = winners.filter((item) => item == 'computer').length
+    let draw = winners.filter((item) => item == 'draw').length
+    console.log('Results:')
+    console.log('Player Wins:', playerWins);
+    console.log('Computer Wins:', computerWins);
+    console.log('Draw:', draw);
+}
+
+function getRound(playerChoice, getComputerChoice, winner, round) {
+    console.log('Round:', round);
+    console.log('Player chose:', playerChoice);
+    console.log('Computer chose:', getComputerChoice);
+    console.log(winner, 'Won the Round');
+    console.log('************');
+}

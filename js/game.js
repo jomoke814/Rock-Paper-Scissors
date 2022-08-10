e2c756fd676ec4ad6600094939aaeeff3e1620a6
jomoke814 <jcullen814@gmail.com>
2022-08-10 13:32:52 +0300

    Refactoring code

--- js/game.js
+++ js/game.js
@@ -15,71 +15,42 @@ function computerPlay () {
 }
 
 function playRound (playerSelection, computerSelection) {
+  const p = document.createElement('p')
   if (playerSelection === computerSelection) {
-    const p = document.createElement('p')
     p.innerText = "it's a draw"
-    outcomeDiv.appendChild(p)
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     computerScore++
-    const p = document.createElement('p')
     p.innerText = 'You lost...Rock covered by Paper'
-    outcomeDiv.appendChild(p)
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     playerScore++
-    const p = document.createElement('p')
     p.innerText = 'You win...Rock crushes Scissors'
-    outcomeDiv.appendChild(p)
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     computerScore++
-    const p = document.createElement('p')
     p.innerText = 'You lost...Scissors cuts Paper'
-    outcomeDiv.appendChild(p)
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     computerScore++
-    const p = document.createElement('p')
     p.innerText = 'You win...Paper covers Rock'
-    outcomeDiv.appendChild(p)
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     computerScore++
-    const p = document.createElement('p')
     p.innerText = 'You lost...Scissors crushed by Rock'
-    outcomeDiv.appendChild(p)
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     computerScore++
-    const p = document.createElement('p')
     p.innerText = 'You win...Scissors cuts Paper'
-    outcomeDiv.appendChild(p)
   }
+  outcomeDiv.appendChild(p)
 }
 
-// function game () {
-//   for (let i = 0; i < 5; i++) {
-//     const playerSelection = prompt('Choose your input', 'Rock, Paper, Scissors').toLowerCase()
-//     const computerSelection = computerPlay()
-//     playRound(playerSelection, computerSelection)
-//   }
-//   if (playerScore > computerScore) {
-//     return '😄Hurrah!...You beat the computer!'
-//   } else if (playerScore < computerScore) {
-//     return '😞What a downer...You got beat by the computer!'
-//   } else {
-//     return "😤It's a Draw."
-//   }
-// }
-
 const checkWinner = (playerScore, computerScore) => {
+  const h2 = document.createElement('h2')
   if(playerScore === 5) {
-    const h2 = document.createElement('h2')
     h2.classList.add('playerWon')
     h2.innerText = `You win ${playerScore} to ${computerScore} you beat the computer`
-    outcomeDiv.appendChild(h2)
   }
   if (computerScore === 5) {
-    const h2 = document.createElement('h2')
     h2.classList.add('computerWon')
     h2.innerText = `You lost ${playerScore} to ${computerScore} you got beat by the computer`
-    outcomeDiv.appendChild(h2)
   }
+  outcomeDiv.appendChild(h2)
 }
 
 const scores = (playerScore, computerScore) => {
